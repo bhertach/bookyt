706d9d9e9dfd0c926e9d724bfa14e47644db46b0
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-05-29 23:47:04 +0200

    Implement multi tenancy.

--- Gemfile
+++ Gemfile
@@ -106,6 +106,7 @@ gem 'omniauth-google-oauth2'
 gem 'faraday'
 gem 'cancan'
 gem 'lyb_devise_admin'
+gem 'apartment'
 
 # Date/Time handling
 gem 'validates_timeliness'
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Bookyt::Application.routes.draw do
   # I18n
   filter 'locale'
 
+  # Tenancy
+  devise_for :admin_users
+
+  namespace :admin do
+    match '/' => redirect('/admin/tenants')
+    resources :tenants
+  end
+
   # Authorization
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
